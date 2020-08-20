8b5560bb97f7206ea490d24829a449b1b915d0d7
Laurent Gautier <lgautier@gmail.com>
2020-08-19 20:43:41 -0400

    Change of layout.

--- build.py
+++ build.py
@@ -5,15 +5,19 @@ import shutil
 
 
 LabelURL = collections.namedtuple('LabelURL', 'label url')
-PROJECTS = (
-    LabelURL('Documentation', 'rpy2_doc.html'),
+MENU = (
+    LabelURL('Documentation', 'doc.html'),
     LabelURL('Docker', 'https://github.com/rpy2/rpy2-docker'),
     LabelURL('MyBinder', 'https://github.com/rpy2/rpy2-mybinder'),
-    LabelURL('R6', 'r6_doc.html'),
-    LabelURL('Matrix', 'https://github.com/rpy2/rpy2-Matrix')
+    LabelURL('Extensions', 'extensions.html'),
 )
-DOC_PARAMS = (
-    ('rpy2_doc.html', 'Documentation',
+SUBPROJECTS = (
+    (LabelURL('R6', 'r6_doc.html'),
+     'Map the R package for Object-Oriented Programming for R of the same name.'),
+    (LabelURL('Matrix', 'https://github.com/rpy2/rpy2-Matrix'),
+     'Map the R package Matrix.')
+)
+DOC_PARAMS = ('doc.html', 'Documentation',
      LabelURL('rpy2', 'https://github.com/rpy2/rpy2'),
      (
          LabelURL('dev', 'doc/latest/html/index.html'),
@@ -23,11 +27,13 @@ DOC_PARAMS = (
          LabelURL('3.0.x', 'doc/v3.0.x/html/index.html'),
          LabelURL('2.9.x', 'doc/v2.9.x/html/index.html')
      )
-    ),
+    )
+
+DOC_SUBPROJECT_PARAMS = (
     ('r6_doc.html', 'R6',
      LabelURL('rpy2-R6', 'https://github.com/rpy2/rpy2-R6'),
      (LabelURL('dev', 'https://rpy2.github.io/rpy2-R6/version/master/html/index.html'),)
-    )
+    ),
 )
 
 assert os.path.isdir('docs')
@@ -43,19 +49,36 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 # render main page
 template = env.get_template('index.html')
-render = template.render(projects=PROJECTS)
+render = template.render(menu=MENU)
 with open(os.path.join('docs', 'index.html'), 'w') as fh:
     fh.write(render)
 
 
 # render documentation pages
 template = env.get_template('doc.html')
-for output, active_label, project, lst_versions in DOC_PARAMS:
+output, active_label, project, lst_versions = DOC_PARAMS
+render = template.render(page_title=project.label,
+                         active_label=active_label,
+                         project=project.label,
+                         project_url=project.url,
+                         doc_versions=lst_versions,
+                         menu=MENU)
+with open(os.path.join('docs', output), 'w') as fh:
+    fh.write(render)
+        
+template = env.get_template('doc_subproject.html')
+for output, active_label, project, lst_versions in DOC_SUBPROJECT_PARAMS:
     render = template.render(page_title=project.label,
                              active_label=active_label,
                              project=project.label,
                              project_url=project.url,
                              doc_versions=lst_versions,
-                             projects=PROJECTS)
+                             menu=MENU)
     with open(os.path.join('docs', output), 'w') as fh:
         fh.write(render)
+
+# render extensions
+template = env.get_template('extensions.html')
+render = template.render(menu=MENU, projects=SUBPROJECTS)
+with open(os.path.join('docs', 'extensions.html'), 'w') as fh:
+    fh.write(render)
