8db9cf57aed7cb7769771b44442925e62dfdde1f
Laurent Gautier <lgautier@gmail.com>
2021-01-10 11:57:39 -0500

    Rebuild for v3.4.2

--- docs/doc/latest/html/_static/bizstyle.js
+++ docs/doc/latest/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("rpy2 3.4.0-dev documentation");
+        $("li.nav-item-0 a").text("rpy2 3.5.0dev documentation");
     }
 });
