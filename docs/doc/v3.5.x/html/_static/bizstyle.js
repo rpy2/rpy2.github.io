3fd00d88e2ea203d5459288a5ae9bda0febeae2f
Laurent Gautier <lgautier@gmail.com>
2022-08-21 17:24:30 -0400

    Rebuild the doc for release 3.5.4.

--- docs/doc/v3.5.x/html/_static/bizstyle.js
+++ docs/doc/v3.5.x/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.3 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.4 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
