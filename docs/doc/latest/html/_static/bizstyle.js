6ba000716cfbce5ebdc42b6ab86f16a1cbdcc4fd
Laurent Gautier <lgautier@gmail.com>
2023-03-05 23:01:12 -0500

    Build for release 3.5.10.

--- docs/doc/latest/html/_static/bizstyle.js
+++ docs/doc/latest/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.7 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.10 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
