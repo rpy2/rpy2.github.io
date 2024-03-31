512e7922f554cda0a72642f5fb9e0a7e4f4e72e1
Laurent Gautier <lgautier@gmail.com>
2024-03-31 17:25:07 -0400

    Rebuild doc for v3.5.16.

--- docs/doc/v3.5.x/html/_static/bizstyle.js
+++ docs/doc/v3.5.x/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.13 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.5.16 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- docs/doc/v3.5.x/html/_static/documentation_options.js
+++ docs/doc/v3.5.x/html/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.5.13',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.5.16',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
