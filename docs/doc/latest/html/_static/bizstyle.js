21ca2a68da400e3a385d4076f577a426b1807db1
Laurent Gautier <lgautier@gmail.com>
2025-06-08 10:48:51 -0400

    Update doc for 3.6.1.

--- docs/doc/latest/html/_static/bizstyle.js
+++ docs/doc/latest/html/_static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.6.0 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "rpy2 3.6.1 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
